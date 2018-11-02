e07d8260d87bfcffcadb8ca1ff1e20a93c988801
Pramit Roy <prroy@linkedin.com>
2018-11-02 14:40:24 -0700

    Adding ceq route

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/ceq', (req, res) => {
+var app = express()
+// var router = express.Router()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/ceq', (req, res) => {
 	res.send('hello');
     }) 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
