17bec80a6b01ede9027b5f5c499f98f5d1c8572c
Pramit Roy <prroy@linkedin.com>
2018-11-02 15:01:28 -0700

    Adding polling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const http = require('http');
 
 var app = express()
 // var router = express.Router()
@@ -9,25 +10,46 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
-app.get('/ceq', (req, res) => {
-  invokeCheck(req);
-}) 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-var invokeCheck = (req) => {
-  var requestLoop = setInterval(function(){
-    request({
-      url: "http://www.google.com",
-      method: "GET",
-      timeout: 10000,
-      followRedirect: true,
-      maxRedirects: 10
-    },function(error, response, body){
-      if(!error && response.statusCode == 200){
-          console.log('sucess!');
-      }else{
-          console.log('error' + response.statusCode);
-      }
-    });
-  }, 60000);  
+const poll = {
+    pollB: function() {
+        http.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.atom', (res) => {
+            const { statusCode } = res;
+
+            let error;
+            if (statusCode !== 200) {
+                error = new Error(`Request Failed.\n` +
+                    `Status Code: ${statusCode}`);
+            }
+
+            if (error) {
+                console.error(error.message);
+                res.resume();
+            } else {
+                res.setEncoding('utf8');
+                let rawData = '';
+                res.on('data', (chunk) => { rawData += chunk; });
+                res.on('end', () => {
+                    try {
+                        const parsedData = JSON.parse(rawData);
+                        console.log("Raw data",parsedData);
+
+                        // The important logic comes here
+                        if (parsedData.status === 'BUSY') {
+                            setTimeout(poll.pollB, 60000); // request again in 10 secs
+                        } else {
+                            // Call the background process you need to
+                        }
+                    } catch (e) {
+                        console.error(e.message);
+                    }
+                });
+            }
+        }).on('error', (e) => {
+            console.error(`Got error: ${e.message}`);
+        });
+    }
 }
+
+poll.pollB();
