ae2491c10e9665bba2b039c9c61a40b1b8abc68f
Pramit Roy <prroy@linkedin.com>
2018-11-02 17:38:40 -0700

    invalid request json

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ var blinkSun = () => {
       path: '/v1/lights/d073d521270c/effects/pulse',
       method: 'POST',
       headers: {
-          Authorization: ' Bearer cc7cf933ebefaf7b47574d219e3d2aa5e8338108e4627b3243d418c58376707b',
+          'Authorization': ' Bearer cc7cf933ebefaf7b47574d219e3d2aa5e8338108e4627b3243d418c58376707b',
           'Content-Type': 'application/json',
           'Content-Length': length
       }
