65933f87421a8483b90512abf9b8ee65e4428520
Pramit Roy <prroy@linkedin.com>
2018-11-08 17:56:42 -0800

    Changing index

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => res.render('pages/start'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 var blinkSun = () => {
