f7639bbba9d972efd5101caac9e227503295422b
Pramit Roy <prroy@linkedin.com>
2018-11-15 18:26:44 -0800

    Fix fire page errors

--- modules/renderFire.js
+++ modules/renderFire.js
@@ -15,15 +15,15 @@ let renderFire = (req, res) => {
             	console.log("ExtractedData", extractedData);
             	if(typeof extractedData != 'undefined' && Object.prototype.toString.call(extractedData) === '[object Array]'){
             		extractedData.forEach((value, index) => {
-            			let link = "";//value['link'][0]
+            			let link = value['link'][0];
             			let title = value['title'][0];
-            			let desc = ""; //value['description'][0];
-            			let lat = ""; //value['geo:lat'][0];
+            			let desc = value['description'][0];
+            			let lat = value['geo:lat'][0];
             			let long = ""; //value['geo:long'][0];
-            			let location = ""; //latlongToDMS(lat, long);
+            			let location = "";//latlongToDMS(lat, long);
             			//console.log("link: ",link);
-            			console.log("title: ",title)
-            			//console.log("lat long: ",lat,long);
+            			//console.log("title: ",title)
+            			console.log("lat long: ",lat,long);
             			//console.log("desc: ",desc);
             			messages.push({
             				link: link,
